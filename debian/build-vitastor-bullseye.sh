5af8eddaa92c2623c64e85d9009b3c9a0af290f9
Vitaliy Filippov <vitalif@yourcmc.ru>
2021-02-26 01:02:32 +0300

    Add the remaining build script for Debian

--- debian/build-vitastor-bullseye.sh
+++ debian/build-vitastor-bullseye.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed 's/$REL/buster/' < vitastor.Dockerfile > ../Dockerfile
+sed 's/$REL/bullseye/' < vitastor.Dockerfile > ../Dockerfile
 cd ..
 mkdir -p packages
 sudo podman build -v `pwd`/packages:/root/packages -f Dockerfile .
