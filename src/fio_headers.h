ebaf4d7a723854a318ea612ff1d6ea78b70820cc
Vitaliy Filippov <vitalif@yourcmc.ru>
2022-01-31 23:39:11 +0300

    Fix compatibility with fio 3.28+

--- src/fio_headers.h
+++ src/fio_headers.h
@@ -1,4 +1,3 @@
-extern "C" {
 // Kill atomics in fio headers
 #define _STDATOMIC_H
 #include "fio/arch/arch.h"
@@ -11,6 +10,7 @@ extern "C" {
 #define CONFIG_HAVE_GETTID
 #define CONFIG_SYNC_FILE_RANGE
 #define CONFIG_PWRITEV2
+extern "C" {
 #include "fio/fio.h"
 #include "fio/optgroup.h"
 }
