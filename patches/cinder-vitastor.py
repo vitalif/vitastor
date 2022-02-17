117d6f061252ef82c851ed92e9fa647ad7919bdb
Vitaliy Filippov <vitalif@yourcmc.ru>
2022-02-17 14:43:44 +0300

    Release 0.6.14
    - Fix IPv6 address parsing
    - Fix "cannot read bytes of undefined" in the monitor on a fresh DB
    - Fix possible hangs of read requests on OSD restarts without immediate_commit=all mode
    - Fix OSDs skipping misplaced recovery in some cases
    - Fix OSDs possibly dying with "map::at" errors when other OSDs are stopped
    - Fix division by zero in ls if all pool OSDs are down

--- patches/cinder-vitastor.py
+++ patches/cinder-vitastor.py
@@ -50,7 +50,7 @@ from cinder.volume import configuration
 from cinder.volume import driver
 from cinder.volume import volume_utils
 
-VERSION = '0.6.13'
+VERSION = '0.6.14'
 
 LOG = logging.getLogger(__name__)
 
