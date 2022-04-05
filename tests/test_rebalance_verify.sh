d334914948ac19590416ca8eacddab6a2dd0dd5b
Vitaliy Filippov <vitalif@yourcmc.ru>
2022-04-06 02:12:51 +0300

    Fix the test so it actually fails indicating a bug :-)

--- tests/test_rebalance_verify.sh
+++ tests/test_rebalance_verify.sh
@@ -8,7 +8,7 @@ $ETCDCTL put /vitastor/config/inode/1/1 '{"name":"testimg","size":'$((IMG_SIZE*1
 
 NBD_DEV=$(sudo build/src/vitastor-nbd map --etcd_address $ETCD_URL --image testimg --logfile ./testdata/nbd.log &)
 
-trap "sudo build/src/vitastor-nbd unmap $NBD_DEV" EXIT
+trap "sudo build/src/vitastor-nbd unmap $NBD_DEV"'; kill -9 $(jobs -p)' EXIT
 
 sudo chown $(id -u) $NBD_DEV
 
@@ -16,13 +16,18 @@ dd if=/dev/urandom of=./testdata/img1.bin bs=1M count=$IMG_SIZE
 
 dd if=./testdata/img1.bin of=$NBD_DEV bs=1M count=$IMG_SIZE oflag=direct
 
+$ETCDCTL put /vitastor/config/inode/1/1 '{"name":"testimg0","size":'$((IMG_SIZE*1024*1024))'}'
+$ETCDCTL put /vitastor/config/inode/1/2 '{"name":"testimg","size":'$((IMG_SIZE*1024*1024))',"parent_id":1}'
+sleep 1
+
 echo "Verifying before rebalance"
 dd if=$NBD_DEV of=./testdata/img2.bin bs=1M count=$IMG_SIZE iflag=direct
 diff ./testdata/img1.bin ./testdata/img2.bin
 
-try_reweight 1 0
-
-try_reweight 2 0
+$ETCDCTL put /vitastor/config/osd/1 '{"reweight":0}'
+$ETCDCTL put /vitastor/config/osd/2 '{"reweight":0}'
+$ETCDCTL put /vitastor/config/osd/3 '{"reweight":0}'
+sleep 1
 
 for i in {1..10000}; do
     O=$(((RANDOM*RANDOM) % (IMG_SIZE*128)))
