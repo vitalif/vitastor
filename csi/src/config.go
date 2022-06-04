101592bbffa194d682b44916e54e2223a8649fb9
Vitaliy Filippov <vitalif@yourcmc.ru>
2022-06-05 00:07:24 +0300

    Release 0.7.1
    - Add ISA-L erasure code implementation, now used automatically instead of jerasure when available
    - Fix listings sending too many parallel requests to OSDs
    - Fix rm-data crashing with --wait-list
    - Remove empty inodes from statistics and `ls` output, after <inode_vanish_time> seconds after deletion
    - Make monitor delete pool statistics when the pool is deleted and thus remove them from `df` output
    - Log multiple etcd addresses in OSD logs correctly
    - Fix true/false parsing in json configs like no_recovery/no_rebalance
    - Show no_recovery, no_rebalance, readonly flags in status

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "0.7.0"
+    vitastorCSIDriverVersion = "0.7.1"
 )
 
 // Config struct fills the parameters of request or user input
