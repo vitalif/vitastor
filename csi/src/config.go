81fc8bb94cd86e672d5bff77a9fabe96a0542439
Vitaliy Filippov <vitalif@yourcmc.ru>
2023-01-13 19:41:34 +0300

    Release 0.8.4
    New features:
    - Implement QCOW2 image/snapshot export via qemu-img (bdrv_co_block_status in the driver)
    - Remove OSDs from PG history during `vitastor-cli rm-osd` to prevent `left_on_dead` PG states after deletion
    - Add a new recovery_pg_switch setting to mix all PGs during recovery, to almost
      fully reduce the probability of ENOSPC during rebalance
    - Introduce partial ENOSPC ("OSD is full") handling - now ENOSPC doesn't turn
      into cascades of crashes
    - Add migration support to Proxmox VE Vitastor driver
    - Track last_clean_pgs on a per-pool basis thus reducing data movement in a cluster
      with pools remaining unclean/degraded for a long time
    
    Bug fixes:
    - Fix a bug where monitor could generate degraded PGs if one of the hosts had no OSDs
    - Fix a bug where monitor could skip PG redistribution with a lot of OSDs in cluster
    - Report PG history synchronously on the first write, which improves PG consistency
      and availability at the same time, because history now gets reported correctly
      and doesn't get reported without the need for it
    - Fix possible write and recovery stalls which could happen in a cluster with both EC and replicated pools
    - Make OSD and monitors sanitize & deduplicate PG history items in etcd
    - Fix non-working OSD peer config safety check
    - Fix a rare journal flush stall where flushing wasn't activated with full journal, but with empty flush queue
    - Fix builds without ISA-L (jerasure-only) crashing with EC N+K, K>=2 due to the lack of 16-byte buffer alignment
    - Fix a possible crash for EC N+K, K>=2 when calculating a parity chunk with previous parity chunk missing
    - Fix a bug where vitastor-disk purge with suppressed warnings didn't work

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "0.8.3"
+    vitastorCSIDriverVersion = "0.8.4"
 )
 
 // Config struct fills the parameters of request or user input
