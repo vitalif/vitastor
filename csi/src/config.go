bdd48e4cf17ca768af01885e61ebf9ca55b60312
Vitaliy Filippov <vitalif@yourcmc.ru>
2023-06-10 11:42:49 +0300

    Release 0.9.1
    - Fix "Client XX command out of sync" messages sometimes happening on OSD reconnections
    - Fix a bug where EC reads parallel with writes to the same object failed with -ERANGE error
    - Slightly reduce the amount of metadata writes during journal flushing
    - Correctly unmap NBD volumes when Proxmox forces map_volume use (with SWTPM and maybe some other cases)

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "0.9.0"
+    vitastorCSIDriverVersion = "0.9.1"
 )
 
 // Config struct fills the parameters of request or user input
--- patches/cinder-vitastor.py
+++ patches/cinder-vitastor.py
@@ -50,7 +50,7 @@ from cinder.volume import configuration
 from cinder.volume import driver
 from cinder.volume import volume_utils
 
-VERSION = '0.9.0'
+VERSION = '0.9.1'
 
 LOG = logging.getLogger(__name__)
 
