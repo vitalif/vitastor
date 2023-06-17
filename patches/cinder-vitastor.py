926be372fd6f5e83edb005fc24987017d330ce55
Vitaliy Filippov <vitalif@yourcmc.ru>
2023-06-18 02:10:39 +0300

    Release 0.9.2
    - Measure and report scrub I/O statistics in vitastor-cli status
    - Make aggregated statistics in vitastor-cli status much smoother
      (first derive, then sum instead of first summing and then deriving)
    - Fix an old rare bug leading to journal corruption
      (try to use scrub if you think you're affected...)
    - Do not start EC PGs without at least <data chunks> OSDs in each old set
      (prevents spurious read errors with EC during reconnections/restarts)
    - Fix failed assert(!scrub_list_op) on OSD restart with pending scrubs
    - Fix future planned scrubs not starting because of incorrect time comparison
    - Build packages for Debian 12 (Bookworm)

--- patches/cinder-vitastor.py
+++ patches/cinder-vitastor.py
@@ -50,7 +50,7 @@ from cinder.volume import configuration
 from cinder.volume import driver
 from cinder.volume import volume_utils
 
-VERSION = '0.9.1'
+VERSION = '0.9.2'
 
 LOG = logging.getLogger(__name__)
 
