a612cdca47dc0dd6f6df2175c0cd5fbb3c532ab8
Vitaliy Filippov <vitalif@yourcmc.ru>
2023-07-01 00:25:58 +0300

    Release 0.9.3
    - Add patch for libvirt 9.0
    - Add support for Proxmox VE 8.0
    - Fix compatibility of the QEMU driver with iothread (QEMU rebuilds are coming)
    - Fix vitastor-cli rm-data/rm/merge hanging when some OSDs are down.
      Allow deletions in unclean cluster at the cost of some data possibly
      "reappearing" when those OSDs start back. In that case you can just repeat
      the deletion request using rm-data.
    - A bunch of bug fixes for snapshots:
      - Fix snapshot reads often not working at all with snapshot chain size > 2
      - Fix optimized snapshot data merge (children to parent)
      - Fix updating of image name index key during optimized merge
      - Fix auto-selection preventing the use of optimized merge with only 1 snapshot
      - Fix incorrect CAS retries during snapshot merge
      - Fix snapshot merge progress reporting
    - Fix primary_read bitmap buffers use-after-free which could lead to
      incorrect allocation map reads
    - Remove /usr/local/bin path from make-etcd
    - Some documentation fixes

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "0.9.2"
+    vitastorCSIDriverVersion = "0.9.3"
 )
 
 // Config struct fills the parameters of request or user input
