016115c0d4febe65a63d03fe3d1d6afdfdf988ee
Vitaliy Filippov <vitalif@yourcmc.ru>
2024-02-04 02:23:49 +0300

    Release 1.4.2
    - Log to systemd by default
    - Fix excessive autosyncs after every operation with disabled immediate_commit (introduced in 1.1.0)
    - Fix a possible write stall with EC due to the lack of OSD wakeup after stabilizing previous writes
    - Change sync operation semantics as a final fix to possible write stalls with EC and disabled immediate_commit
    - Sync after deleting data in CLI rm / rm-data if immediate_commit is disabled
    - Fix OSDs ignoring syncs & autosyncs for delete operations
    - Fix OSD space reporting sometimes adding garbage zeros for deleted inodes (causing extra pool/stats etcd keys for deleted pools)
    - Speed up monitor failover - change default etcd_mon_ttl from 30 to 5 seconds
    - Speed up operation retries - change default up_wait_retry_interval to 50 ms
    - Add patch for libvirt 9.10

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "1.4.1"
+    vitastorCSIDriverVersion = "1.4.2"
 )
 
 // Config struct fills the parameters of request or user input
