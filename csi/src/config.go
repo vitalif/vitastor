7fbb04fdfa89cd035257860a7d42e014570bfc12
Vitaliy Filippov <vitalif@yourcmc.ru>
2024-12-19 11:45:12 +0300

    Release 1.10.0
    New features:
    
    - Implement basic VitastorFS support in [CSI](https://vitastor.io/docs/installation/kubernetes.html)
    - Implement [NFS RDMA](https://vitastor.io/docs/usage/nfs.html#rdma) support
    - Pause pool rebalance when monitor detects that it can lead to any OSD becoming full ([osd_backfillfull_ratio](https://vitastor.io/docs/config/monitor.html#osd_backfillfull_ratio))
    - Auto-select correct [RDMA device and GID](https://vitastor.io/docs/config/network.html#rdma_device) based on osd_network and RoCEv2 priority
    - Report slow ops in OSD stats in etcd and show them in vitastor-cli status
    
    Bug fixes:
    
    - Fix possibly incorrect linked list deserialization in NFS
    - Fix possible crash in vitastor-nfs --block READDIR operation
    - Map netlink after forking to show correct PID in vitastor-nbd ls
    - Simplify and fix create-pool OSD count checks for the case of hosts split into sub-nodes
    - Make monitor print "Waiting to become master" just once, not every 5s
    - Take out_size from oimg if not specified in vitastor-cli dd
    - Do not report OSDs with empty statistics as "full" in status
    - Trigger double autosync when switching PG state to prevent leaving garbage in non-immediate_commit clusters
    - Fix a lack of connection timeout for etcd websockets in OSD leading to slower etcd failover (~70s instead of ~10s)
    - Fix a rare OSD crash during client disconnect
    - Fix PGs sometimes sticking until OSD restart in the "has_unclean" state with EC pools
    - Fix metadata partition zeroing in vitastor-disk prepare
    - Add patches for qemu 9.1 and pve-qemu 9.0 and 9.1
    - Fix libvirt 8 patch

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "1.9.3"
+    vitastorCSIDriverVersion = "1.10.0"
 )
 
 // Config struct fills the parameters of request or user input
--- patches/cinder-vitastor.py
+++ patches/cinder-vitastor.py
@@ -50,7 +50,7 @@ from cinder.volume import configuration
 from cinder.volume import driver
 from cinder.volume import volume_utils
 
-VITASTOR_VERSION = '1.9.3'
+VITASTOR_VERSION = '1.10.0'
 
 LOG = logging.getLogger(__name__)
 
