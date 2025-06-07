97bb809b54f71e0e41ca3a695caa509e93ca5fbd
Vitaliy Filippov <vitalif@yourcmc.ru>
2025-06-07 12:56:31 +0300

    Release 2.2.2
    - Fix a bug introduced in 2.2.0 - pg_locks weren't disabled for pools without local_reads
      correctly which could lead to inactive pools during various operations
    - Fix an old bug where OSDs could send sub-operations to incorrect peer OSDs when their
      connections were stopped and reestablished quickly, in 2.2.0 it was usually leading
      to "sequencing broken" messages in OSD logs
    - Fix debug use_sync_send_recv mode

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "2.2.1"
+    vitastorCSIDriverVersion = "2.2.2"
 )
 
 // Config struct fills the parameters of request or user input
