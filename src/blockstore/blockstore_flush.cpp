84961f6d0a2ad30d1533c7915c85d14298a9d708
Vitaliy Filippov <vitalif@yourcmc.ru>
2025-07-06 18:45:06 +0300

    Fix repeating cur_oid

--- src/blockstore/blockstore_flush.cpp
+++ src/blockstore/blockstore_flush.cpp
@@ -162,6 +162,7 @@ bool journal_flusher_co::loop()
     else if (wait_state == 19) goto resume_19;
     else if (wait_state == 20) goto resume_20;
 resume_0:
+    cur_oid = {};
     res = bs->heap->get_next_compact(cur_oid);
     if (res == ENOENT)
     {
