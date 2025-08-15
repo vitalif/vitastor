d77a77594838711ff066bcc10df3596399bcb6ff
Vitaliy Filippov <vitalif@yourcmc.ru>
2025-08-16 01:21:43 +0300

    Fix liburing include/compat for older kernels

--- src/liburing/include/liburing/compat.h
+++ src/liburing/include/liburing/compat.h
@@ -2,12 +2,26 @@
 #ifndef LIBURING_COMPAT_H
 #define LIBURING_COMPAT_H
 
+#include <linux/version.h>
+
 #include <linux/time_types.h>
 /* <linux/time_types.h> is included above and not needed again */
 #define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
 
 #include <linux/openat2.h>
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,12,0)
+
 #include <linux/blkdev.h>
+
+#else
+
+#include <linux/ioctl.h>
+
+#ifndef BLOCK_URING_CMD_DISCARD
+#define BLOCK_URING_CMD_DISCARD                        _IO(0x12, 0)
+#endif
+
+#endif
 
 #endif
