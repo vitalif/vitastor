c20450c1f1de4c0f694944312e22f78e617bd94b
Vitaliy Filippov <vitalif@yourcmc.ru>
2025-08-24 22:15:55 +0300

    Add build.sh for Debian Trixie

---
+++ debian/build-vitastor-trixie.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build --build-arg DISTRO=debian --build-arg REL=trixie -t vitastor-buildenv:trixie -f vitastor-buildenv.Dockerfile .
+docker run -i --rm -e REL=trixie -v `dirname $0`/../:/root/vitastor vitastor-buildenv:trixie /root/vitastor/debian/vitastor-build.sh
