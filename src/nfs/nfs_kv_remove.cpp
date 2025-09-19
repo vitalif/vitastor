c4491b07b8e70f5efedfa5130ec5cdf7eaeaeddd
ston3lu <ston3lu@163.com>
2025-09-19 09:11:16 +0000

    change delete operation to set for restoring direntry in nfs_kv_remove

--- src/nfs/nfs_kv_remove.cpp
+++ src/nfs/nfs_kv_remove.cpp
@@ -199,7 +199,7 @@ resume_4:
         else
         {
             // Not OK, restore direntry
-            st->self->parent->db->del(kv_direntry_key(st->dir_ino, st->filename), [st](int res)
+            st->self->parent->db->set(kv_direntry_key(st->dir_ino, st->filename), st->direntry_text, [st](int res)
             {
                 st->res2 = res;
                 nfs_kv_continue_delete(st, 5);
