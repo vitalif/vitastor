cb639a130d97761fac55cfc6b886baa147acf477
Vitaliy Filippov <vitalif@yourcmc.ru>
2026-03-07 13:41:28 +0300

    Release 3.0.5
    A single urgent fix for the new store: metadata was written incorrectly when
    filled metadata area exceeded 4 GB due to a simple & stupid 32-bit integer overflow.
    This was leading to OSDs corrupting metadata and being unable to start on the next
    run.
    
    The bug affected only the new store (meta_format=3) and OSDs with metadata area size
    larger than 4 GB. You can check your OSD metadata area size by checking `data_offset`
    in `vitastor-disk read-sb /dev/vitastor/osdXX-data` for single-disk OSDs or
    `blockdev --getsize64 /dev/vitastor/osdXX-meta` for hybrid OSDs.
    
    If you're affected, it's highly recommended to:
    - Install updated packages without restarting OSDs
    - Restart OSDs **one by one**
    - If a restart of an OSD triggers "has_degraded" rebalance - wait for it to finish
      before restarting another OSD
    - If an OSD fails to start with "double-claimed" errors in the log - purge and recreate it,
      and again wait for "has_degraded" rebalance to finish before proceeding to the next OSD

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "3.0.4"
+    vitastorCSIDriverVersion = "3.0.5"
 )
 
 // Config struct fills the parameters of request or user input
