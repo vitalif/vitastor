538620b400ac07dc0475a1a1e7e35921a521fbd7
Vitaliy Filippov <vitalif@yourcmc.ru>
2026-03-22 21:43:38 +0300

    Release 3.0.6
    - Fix one more bug in old store checksums O_o - checksum verification wasn't
      working correctly on reads quickly after small initial offsetted writes
      (when a BIG_WRITE was read from the journal).
    - Fix clients possibly not destroyed after being switched to RDMA. Not 100%
      verified, but in theory it could rarely lead to PGs hanging in peering/stopping
      states with RDMA enabled.
    - Use vitastor-cli in the Cinder driver for modifications instead of direct etcd
      interaction.
    - Fix mon_https_ca parsing.
    - Fix Docker install.sh, add instructions for Podman.

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "3.0.5"
+    vitastorCSIDriverVersion = "3.0.6"
 )
 
 // Config struct fills the parameters of request or user input
