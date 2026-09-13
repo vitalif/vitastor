06a5d2424058ed2656501b766675e08f988cade5
Vitaliy Filippov <vitalif@yourcmc.ru>
2026-09-13 11:11:12 +0300

    Release 3.2.1
    - Blockstores
      - Add more asynchrony to the chaos test to uncover more issues...
      - ...and fix them (~6 in the old store, ~9 in the new store)
      - Fix size check for journal_offset when sharing meta device (thanks Changwei Ye)
    - Network layer
      - Correctly handle out-of-order zero-copy notifications (rare but possible in theory)
      - Fix double checksumming of header in proto_checksums=full mode
      - Add rxbounce option for Windows guests with TCP and enabled checksums
      - Close stopped peer sockets early to prevent hung zerocopy notifications and slow failover (~15 min instead of ~10 sec)
    - OSD
      - Do not die on local blockstore modification errors (irrelevant since added write error handling)
    - CSI
      - Fix VitastorFS NFS daemon lifecycle (thanks Dmitry Naumov)
      - Use a newer qemu-storage-daemon in CSI
    - vitastor-cli
      - Add `dd` opool option
      - Fix `rm` with "inverse" optimisation incorrectly removing layer data before rename
        possibly leading to incorrect reads from child images during removal of an older-than-latest snapshot
      - Fix a possible hang in `merge` and derived commands (`rm`, `flatten`)
      - Fix mistakes in the `serve` OpenAPI spec (thanks Stepan Rabotkin)
      - Fix possible segfaults in `serve` due to invalid request parsing (thanks Vincent Schweiger)
      - Fix `serve` no-HTTPS mode (thanks both :))
    - Build
      - Fix non-x86 builds failing due to unsupported xxhash x86dispatch
      - Forcibly enable assertions in all builds to remove build profile dependency
      - Fix QEMU driver build with QEMU < 10.2

--- csi/src/config.go
+++ csi/src/config.go
@@ -5,7 +5,7 @@ package vitastor
 
 const (
     vitastorCSIDriverName    = "csi.vitastor.io"
-    vitastorCSIDriverVersion = "3.2.0"
+    vitastorCSIDriverVersion = "3.2.1"
 )
 
 // Config struct fills the parameters of request or user input
